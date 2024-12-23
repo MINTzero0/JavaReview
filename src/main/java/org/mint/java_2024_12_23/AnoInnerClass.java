cfde3b1ce6d55897dbbfb98f0fba8c885b6508ee
Mint <3522410297@qq.com>
2024-12-23 21:12:34 +0800

    12.23日练习

--- src/main/java/org/mint/java_2024_12_23/AnoInnerClass.java
+++ src/main/java/org/mint/java_2024_12_23/AnoInnerClass.java
@@ -7,6 +7,28 @@ package org.mint.java_2024_12_23;
 
 public class AnoInnerClass {
     public static void main(String[] args) {
+        DD dd = new DD();
+        dd.method();
+    }
+}
 
+interface EE{
+    void method();
+}
+
+class DD{
+    public void method(){
+        EE ee = new EE() {
+            //编译类型是EE
+            //运行类型是DD$1
+
+            @Override
+            public void method() {
+                System.out.println("匿名内部类");
+            }
+        };
+
+        ee.method();
+        System.out.println(ee.getClass());//DD$1
     }
 }
